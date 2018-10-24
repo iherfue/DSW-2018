2aed7c6d5afd2693a5d52863444e3d5b32054458
Ivan Hernandez Fuentes <32276409+iherfue@users.noreply.github.com>
2018-10-24 10:37:26 +0100

    arreglado

--- A19/ejercicio4/nuevo-1.php
+++ A19/ejercicio4/nuevo-1.php
@@ -1,15 +1,10 @@
-<?php
-session_start();
-
-?>
-
 <html>
 <body>
   Escriba el nuevo dato:
   <form action="nuevo-2.php" method="GET">
-    Nombre del dato<input type="text" name="nombre_dato">
-    Valor del dato<input type="text" name="valor_dato">
-    <input type="submit" name="enviar" value="guardar">
+    Nombre del dato<input type="text" name="nombre">
+    Valor del dato<input type="text" name="valor">
+    <input type="submit" value="guardar">
   </form>
 </body>
 </html>

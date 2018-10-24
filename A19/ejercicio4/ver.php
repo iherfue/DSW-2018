964ac903abb417aeae25f916560a977859c80bd4
Ivan Hernandez Fuentes <32276409+iherfue@users.noreply.github.com>
2018-10-24 10:38:41 +0100

    arreglado

--- A19/ejercicio4/ver.php
+++ A19/ejercicio4/ver.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
-if(isset($_SESSION['nombre']) && isset($_SESSION['valor'])){
+if(count(($_SESSION))){
 
 echo "<p>Datos Introducidos:</p>";
-echo $_SESSION['nombre'] . ': ' . $_SESSION['valor'];
+echo "<ul>";
+
+foreach ($_SESSION as $indice => $valor) {
+
+    echo " <li>$indice: $valor</li>";
+}
+echo "</ul>";
 echo "<br/> <a href='index.php'>Volver al inicio</a>";
 }else{
   echo "No se han introducido datos";
